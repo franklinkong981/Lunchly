54454645b189c9b38fd3ee6150da09391d2cad9c
Franklin Kong <franklinkong981@gmail.com>
2024-11-09 12:18:25 -0800

    Made minor changes to starter code for proper setup

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const nunjucks = require("nunjucks");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 const ExpressError = require("./errors/expressError");
